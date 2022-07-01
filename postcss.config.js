4652e4855930cdfcc9a5db271647c79267591b26
lencepalard <41090126+lencepalard@users.noreply.github.com>
2022-07-01 11:06:32 +0800

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  
+  },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  }
+
+}
